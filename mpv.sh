024ddfca9f001de5fd33d4ae8bf974cf25f8d56a
Kogasa <kogasapls@gmail.com>
2022-01-14 10:46:12 -0600

    comment mpv.sh

--- mpv.sh
+++ mpv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 tree="$(swaymsg -t get_tree)"
 
+## Since rearranging is hard, we prefer to move everything from one workspace
+## to a clean one. We use 11 and 12 for our mpv monitor.
 if [ -z "$(echo $tree | grep '"current_workspace": "11",')" ]; then
   sourceWS=12
   targetWS=11
@@ -9,17 +11,19 @@ else
   targetWS=12
 fi
 
-numWins="$(echo "$tree" | grep '"app_id": "mpv"' -c)"
+numWins="$(echo "$tree" | grep '"app_id": "mpv"' -c)" #number of mpv clients
 
 swaymsg unmark
-swaymsg [app_id="chatterino"] mark chat
-swaymsg [app_id="firefox" title=".*Chat.*Destiny\.gg"] mark dgg
+swaymsg [app_id="chatterino"] mark chat                         #top chat
+swaymsg [app_id="firefox" title=".*Chat.*Destiny\.gg"] mark dgg #bottom chat
+
 swaymsg [con_mark="chat"] move container to workspace "$targetWS"
-if (("$numWins" == 0)); then #if there's no mpv windows to position, use something...
+if (("$numWins" == 0)); then #if there's no mpv windows to position, use something... WTB placeholders pls
   swaymsg [workspace=$sourceWS] mark win1
 else
   swaymsg [workspace="$sourceWS" app_id="mpv"] mark win1
 fi
+# Put win1 to the left of chat(s) with width 1920px
 swaymsg [con_mark="chat"] splith
 swaymsg [con_mark="win1"] move to mark chat
 swaymsg [con_mark="win1"] swap container with mark chat
@@ -32,6 +36,7 @@ if (("$numWins" <= 1)); then
   exit
 fi
 
+# Put win2 above win1, so that win1 has size 1920x1080
 swaymsg [workspace="$sourceWS" app_id="mpv"] mark win2
 swaymsg [con_mark="win1"] splitv
 swaymsg [con_mark="win2"] move to mark win1
@@ -43,6 +48,7 @@ if (("$numWins" == 2)); then
   exit
 fi
 
+# Put win3 next to win2 so they're evenly split
 swaymsg [workspace="$sourceWS" app_id="mpv"] mark win3
 swaymsg [con_mark="win2"] splith
 swaymsg [con_mark="win3"] move to mark win2
@@ -53,6 +59,7 @@ if (("$numWins" == 3)); then
   exit
 fi
 
+# Put win4 next to win2 and win3 and then size them evenly
 swaymsg [workspace="$sourceWS" app_id="mpv"] mark win4
 swaymsg [con_mark="win3"] splith
 swaymsg [con_mark="win4"] move to mark win3
